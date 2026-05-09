3284ee9f3fa23b3454a52cc35bd4f8ed767f6b91
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-09 16:12:59 +0500

    fix: replace fragment with div for consistent layout structure

--- AiApp/src/App.jsx
+++ AiApp/src/App.jsx
@@ -1,9 +1,9 @@
 import AIApp from "./components/AIApp.jsx";
 function App() {
   return (
-    <>
+    <div className="h-screen text-white flex flex-col">
       <AIApp />
-    </>
+    </div>
   );
 }
 
